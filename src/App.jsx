e1ae38bb102d6dd96ad519d61aa43b4cfdd6a697
Chaakkii <tsangchakfung@gmail.com>
2024-04-03 15:39:15 +0300

    välipush

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
 
-  return (
-    <>
-      <h1>Query</h1>
-      <div className="card">
-        <button>
-          New Query
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const fetchQuestion = () => {
+    fetch('localhost8080/api/queries/1/questions')
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error in fetch: " + response.statusText);
+
+        return response.json();
+
+  })
+  .then(data => setData(data._embedded.questions))
+  .catch(err => console.error(err))
+
+}
+
+
+
 }
 
 export default App
