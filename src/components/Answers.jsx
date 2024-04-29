c5e42bf5cb774aa25614127da202e86e13f1b6fe
pinzati <pinskutirkkonen@hotmail.com>
2024-04-29 11:59:07 +0300

    desc lisäys answereihin

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -8,46 +8,48 @@ function Answers() {
     const { from } = location.state;
     const [query, setQuery] = useState([]);
 
-  useEffect(() => {
+    useEffect(() => {
         setQuery(from);
-    }, []); 
+    }, []);
 
-   return (
-    <>
-    {query.questions && query.questions.length > 0 ? (
-            <div>
-            <h1>{query.name}</h1>
-            
-            <table className="center">
-                <tbody>
-                    {query.questions.map((question) => (
-                        <React.Fragment key={question.questionid}>
-                            <tr>
-                                <td className="myTD">{question.questionText}</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <ul className="myUL">
-                                        {question.answers.map(answer => (
-                                            <li key={answer.answerid}>
-                                                {answer.answerText}</li>
-                                        ))}
-                                    </ul>
-                                </td>
-                            </tr>
-                        </React.Fragment>
-                    ))}
-                </tbody>
-            </table>
-            </div>
-    ) : ( 
-        <p>Ei vastauksia saatavilla!</p>
-        
-    )}
-             <Link to='/'><button>Back to queries</button></Link>
+    return (
+        <>
+            <Link to='/'><button>Back to queries</button></Link>
 
-    </>
-)
+            {query.questions && query.questions.length > 0 ? (
+                <div>
+                    <h1>{query.name}</h1>
+
+                    <p>{query.desc}</p>
+
+                    <table className="center">
+                        <tbody>
+                            {query.questions.map((question) => (
+                                <React.Fragment key={question.questionid}>
+                                    <tr>
+                                        <td className="myTD">{question.questionText}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <ul className="myUL">
+                                                {question.answers.map(answer => (
+                                                    <li key={answer.answerid}>
+                                                        {answer.answerText}</li>
+                                                ))}
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </React.Fragment>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <p>Ei vastauksia saatavilla!</p>
+
+            )}
+        </>
+    )
 }
 
 export default Answers;
